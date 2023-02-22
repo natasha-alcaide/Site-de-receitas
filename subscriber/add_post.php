affea7d12c11ab7c944f474eb6c01bcab7dae857
Melissa Oliveira <melissaoliveira.mso@gmail.com>
2023-02-21 22:42:20 -0300

    correção de bug em add_post do editor

--- subscriber/add_post.php
+++ subscriber/add_post.php
@@ -84,7 +84,7 @@ if (isset($_POST['create_post'])) {
                 $cat_id = $row['cat_id'];
                 $cat_title = $row['cat_title'];
 
-                echo "<option value='{$cat_id}'>{$cat_title}></option>";
+                echo "<option value='{$cat_id}'>{$cat_title}</option>";
               }
 
               ?>
